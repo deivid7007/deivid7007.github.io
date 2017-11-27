4741628500f96daad979cb37409a20f0761c8c8b
deivid7007 <deivid7007@mail.bg>
2017-11-28 00:20:23 +0200

    Testing windows close logic new implementation on the server.

--- JS/Menu.js
+++ JS/Menu.js
@@ -8,6 +8,7 @@ $(document).ready(function ()
     {
         if(confirm("Are you sure you want to close the page ?"))
         {
+            window.open('', '_self', '');
             window.close();
         }
     });
